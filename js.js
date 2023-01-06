70c3d4886426b2f463f8b5ea03bf444da711a3ab
Mashaaaap <masha.gapen@gmail.com>
2023-01-06 13:46:10 +0200

    homework10

--- js.js
+++ js.js
@@ -85,24 +85,26 @@
 //     alert('Hello')
 // })
 
-let h1st = document.querySelector('h1');
-let unList = document.querySelector('ul');
-let inputThing = document.querySelector('#input');
-let btn = document.querySelector('#btn');
+// let h1st = document.querySelector('h1');
+// let unList = document.querySelector('ul');
+// let inputThing = document.querySelector('#input');
+// let btn = document.querySelector('#btn');
 
-btn.addEventListener('click', () => {
-if (inputThing.ariaValueMax.length != 0){
-    unList.innerHTML += `
-    <li>
-       ${inputThing.value}
-    </li>
-    `
-    inputThing.value ="";
-}
-} );
-inputThing.addEventListener('keypress', (event) =>{
-    h1st.textContent = event.key;
-})
+// btn.addEventListener('click', () => {
+// if (inputThing.ariaValueMax.length != 0){
+//     unList.innerHTML += `
+//     <li>
+//        ${inputThing.value}
+//     </li>
+//     `
+//     inputThing.value ="";
+// }
+// } );
+// inputThing.addEventListener('keypress', (event) =>{
+//     h1st.textContent = event.key;
+// })
+
+//№10
 
 // let notesBlock = document.querySelector('#notesBlock');
 // let btnCreate = document.querySelector('.btnCreate');
@@ -162,3 +164,168 @@ inputThing.addEventListener('keypress', (event) =>{
 // }
 
 // btnCreate.addEventListener('click', createNote);
+
+// clock
+
+// let inputSeconds = document.querySelector('#inputSeconds')
+// let inputMinutes = document.querySelector('#inputMinutes')
+// let inputHours = document.querySelector('#inputHours')
+// let btnStart = document.querySelector('#btnStart')
+// let body = document.querySelector('#body')
+
+// let flag = true
+// btnStart.addEventListener('click', () => {
+//     let seconds = inputSeconds.value
+//     setTimeout (() => {
+//         seconds--
+//         if(true){
+//             document.querySelector('#Time').innerHTML = seconds
+//         }
+//     }, 1000)
+//     setTimeout (() => {
+//         flag = false
+//         body.style.backgroundColor = '#000'
+//     }, inputSeconds.value*1000)
+
+//     let minutes = inputMinutes.value
+//     setTimeout (() => {
+//         minutes--
+//         if(true){
+//             document.querySelector('#Time').innerHTML += minutes
+//         }
+//     }, 60000)
+//     setTimeout (() => {
+//         flag = false
+//         body.style.backgroundColor = '#000'
+//     }, inputSeconds.value*60000)
+
+// })
+
+// let seconds = inputSeconds.value
+// let minutes = inputMinutes.value
+// let hours = inputHours.value
+// function time (hours, minutes, seconds){
+//    if (seconds == 0 && minutes == 0 && hours == 0){
+//     body.style.backgroundColor = '#000'
+//    }
+// }
+
+
+
+let notesHtml = document.querySelector('#notes')
+let btnPost = document.querySelector('#btnPost')
+let inputSearch = document.querySelector('#inputSearch')
+let inputs = { 
+    title: document.querySelector('#inputTitle'),
+    text: document.querySelector('#inputText'),
+    color: document.querySelector('#inputColor'),
+    clearInput(){
+        this.title.value = null
+        this.text.value = null
+    },
+
+    getInfo() {
+        if(this.title.value.length != 0 && this.text.value.length != 0){
+            return {
+                title: this.title.value,
+                text: this.text.value,
+                color: this.color.value
+            }
+        } else { 
+            return null
+        }
+    }
+}
+
+let notesStore = [
+    {
+        id:1,
+        title: 'Title 1',
+        text: 'Do h/w',
+        color: '#FFF',
+        timeCreated: {
+            hours: 10,
+            minutes: 20,
+            seconds: 25
+        }
+    }
+]
+
+let btnsDelete = []
+let btnsChange = []
+
+let genereteNotes = () => { 
+    notesHtml.innerHTML = null
+    btnsDelete = [] 
+    btnsChange = []
+    notesStore.map((el,index) => {
+        if(!el.statusDelete){
+        notesHtml.innerHTML += `
+        <div class="noteBlock" id="note${index}" style="background:${el.color}">
+            <h2>${el.title}</h2>
+            <p id="noteText${index}">${el.text}</p>
+            <p class="time">${el.timeCreated.hours}:${el.timeCreated.minutes}:${el.timeCreated.seconds}</p>
+            <button id="btnChange${index}"><img class="icon" src="./edit.svg" alt="change"/></button>
+            <button class="btnDelete" id="btnDel${index}">Delete</button>
+        </div>
+        `     
+        }
+        //console.log(document.querySelector(`#note${index}`))  
+      
+    })
+    
+    for(let i=0;i<=notesStore.length-1;i++){
+        if(!notesStore[i].statusDelete){
+            btnsDelete[i] = document.querySelector(`#btnDel${i}`)
+            btnsDelete[i].addEventListener('click', () => { 
+                notesStore[i].statusDelete = true
+                genereteNotes()
+            })
+            
+            btnsChange[i] = document.querySelector(`#btnChange${i}`)
+            btnsChange[i].addEventListener('click', () => {
+                document.querySelector(`#note${i}`).innerHTML = `
+                <textarea id="changeTitle${i}" placeholder="Write new text"></textarea>
+                <textarea id="change${i}" placeholder="Write new title"></textarea>
+                <button id="btnSave${i}"><img class="icon" src="./save.svg" alt="change"/></button>
+                <button class="btnDelete" id="btnDel${i}">Delete</button>
+                `
+                document.querySelector(`#btnSave${i}`).addEventListener('click', () => {
+                    let inputChange = { 
+                        title: document.querySelector(`#change${i}`),
+                        text: document.querySelector(`#changeTitle${i}`)
+                    }
+                    if (inputChange.title.value.length != 0){
+                        notesStore[i].text = inputChange.text.value
+                    }
+                    if (inputChange.text.value.length !=0) {
+                        notesStore[i].title = inputChange.title.value
+                    }
+                    genereteNotes()
+                })
+            })
+        }
+    }
+}
+
+let btnPostClick = () => { 
+    if(inputs.getInfo()) { 
+        notesStore = [
+            ...notesStore, 
+            {   
+                id: notesStore.length+1,
+                title: inputs.getInfo().title,
+                text: inputs.getInfo().text,
+                color: inputs.getInfo().color
+            }
+        ]
+        inputs.clearInput()
+        genereteNotes()
+    } else { 
+        alert("Pls, write title or text")
+    }
+}
+
+genereteNotes()
+
+btnPost.addEventListener('click', btnPostClick)
